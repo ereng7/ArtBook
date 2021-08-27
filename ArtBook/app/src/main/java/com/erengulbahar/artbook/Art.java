864c474b1cf38e0f10402bd8189d4cae1108495b
Eren Gülbahar <erenngulbahar@gmail.com>
2021-08-27 16:15:00 +0300

    Art Book

---
+++ ArtBook/app/src/main/java/com/erengulbahar/artbook/Art.java
@@ -0,0 +1,13 @@
+package com.erengulbahar.artbook;
+
+public class Art
+{
+    String name;
+    int id;
+
+    public Art(String name, int id)
+    {
+        this.name = name;
+        this.id = id;
+    }
+}
